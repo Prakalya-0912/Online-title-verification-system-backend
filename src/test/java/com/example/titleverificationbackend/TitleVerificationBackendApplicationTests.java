edd55e685a3db917dd873c3b7e169c5337d1cfd4
Prakalya Singaravelu <kstharunvenkat6tharun@gmail.com>
2026-08-03 21:55:19 +0530

    Initial backend commit

---
+++ src/test/java/com/example/titleverificationbackend/TitleVerificationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.titleverificationbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitleVerificationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
